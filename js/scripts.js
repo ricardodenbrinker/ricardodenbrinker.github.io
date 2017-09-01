b4108ad2705f4fd263fd0a59508ae4d72001998f
ricardodenbrinker <rdb@viisi.nl>
2017-09-01 14:40:13 +0200

    Eerstecommit

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+$('.my-class').delay(3000).fadeIn(500);
+});
+
+$(document).ready(function() {
+$('.my-class1').delay(3300).fadeIn(500);
+});
+
+$(document).ready(function() {
+$('.my-class2').delay(3600).fadeIn(500);
+});
+
+$(document).ready(function() {
+$('.my-class3').delay(3900).fadeIn(500);
+});
+
+$(document).ready(function() {
+$('.my-class4').delay(4200).fadeIn(500);
+});
+
+$(document).ready(function() {
+$('.my-class5').delay(4500).fadeIn(500);
+});
+
+$(document).ready(function() {
+$('.my-class6').delay(8000).fadeIn(500);
+});
